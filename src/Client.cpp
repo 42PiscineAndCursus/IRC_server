6463e76dc42a3141636796638a4765cb3db082e3
daeungkim <daeungkim93@gmail.com>
2020-11-18 16:00:58 +0900

    etc:analyze client
    <description>
    analyze client
    <issue-number>

--- src/Client.cpp
+++ src/Client.cpp
@@ -44,12 +44,18 @@ int Client::setClient()
 		type = "client";
 	else
 		return -1;
+	// 클라이언트의 nick 과 user설정이 완료되면 client를 설정함
 	make_prefix();
 	prefix = std::string(":").append(nick);
+	// 왜 make_prefix함수를 쓰는지 모르겠음
+	// make_prefix에서 설정한 값은 prefix = std::string(":").append(nick);에서 사라짐
 	prefix.append("!");
 	prefix.append(username);
 	prefix.append("@");
 	prefix.append(ip);
+	// prefix설정
+	// nick = dakim0 user = dakim0인경우 호스트 ip = 169.254.44.86인경우
+	// prefix = :dakim0!dakim0@169.254.44.86 로 설정됨
 	return (0);
 }
 
@@ -59,5 +65,7 @@ void Client::make_prefix()
 	prefix = std::string(":").append(nick);
 	prefix.append("!~");
 	prefix.append(username);
+	// nick = dakim0 user = dakim0인 경우
+	// prefix = :dakim0!~dakim0으로 설정됨
 	return;
 }
